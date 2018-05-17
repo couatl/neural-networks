4427410211a066ecdbcc051aa93cab067e789bfa
Svoykina N <nadya.svojkina@gmail.com>
2018-05-18 00:28:43 +0300

    Added lab on back propagation

---
+++ 6-back_propagation/main.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Вариант 16
+
+import sys
+
+from matplotlib import pyplot
+from perceptron import Network
+
+import math
+
+
+def f(net):
+    """
+    Фунция активации
+    :param net: комбинированный вход
+    """
+    return (1 - math.exp(-net)) / (1 + math.exp(-net))
+
+
+def df(net):
+    """
+    Производная функции активации
+    :param net: комбинированный вход
+    """
+    f = (1 - math.exp(-net)) / (1 + math.exp(-net))
+    return (1 - f ** 2) / 2
+
+
+def start(N, J, M, x, t):
+    """
+    Функция запуска обучающего цикла у МНС
+    """
+
+    learn_k = 0.9
+    network = Network(N, J, M, x, t, learn_k, f, df)
+
+    network.learning()
+
+
+if __name__ == '__main__':
+
+    start_text = '*********************************** Лабораторная 6. **********************************' \
+                 '\n Изучение алгоритма обратного распространения ошибки (метод Back Propagation) ' \
+                 '\nВведите команду:' \
+                 '\n   start                -- начать обучение' \
+                 '\n   help                 -- вывод списка комманд' \
+                 '\n   exit                 -- выход из программы' \
+                 '\n'
+
+    print(start_text)
+
+    # Размер входного слоя
+    N = 2
+
+    # Размер скрытого слоя
+    J = 1
+
+    # Размер выходного слоя
+    M = 2
+
+    # Входной вектор
+    x = [1, 2]
+
+    # Целевой вектор
+    t = [-1, -1]
+
+    while 1:
+        command = raw_input()
+
+        if command == 'start':
+            start(N, J, M, x, t)
+        elif command == 'help':
+            print(start_text)
+        elif command == '' or command == 'exit':
+            break
+        else:
+            print('Некорректный ввод!')
---
+++ 6-back_propagation/perceptron.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import math
+
+
+class Network:
+    """
+    Многослойная Нейронная Сеть с одним скрытым слоем
+    """
+
+    def __init__(self, N, J, M, x, t, learn_k, f, df):
+        """
+        Конструктор класса
+        :param N: Размер входного слоя
+        :param J: Размер скрытого слоя
+        :param M: Размер выходного слоя
+        :param learn_k: норма
+        :param f: Функция активации
+        :param df: Производная функции активации
+        :param x: Входной вектор
+        :param t: Обучающая выборка
+        """
+        self.N = N
+        self.J = J
+        self.M = M
+        self.f = f
+        self.df = df
+        self.x = x
+        self.t = t
+        self.learn_k = learn_k
+
+        self.epoch = 0
+
+        # Первые N значений в первый нейрон следующего слоя, вторые N во второй и т.д.
+        self.weight_input = [[0 for x in range(N)] for y in range(J)]
+        self.weight_hidden = [[0 for x in range(J)] for y in range(M)]
+
+    def calculate_output(self, x):
+        """
+        Вычисление выхода МНС по заданному входному сигналу x
+        :param x: N входных сигналов // массив длины N
+        :return: M выходных сигналов
+        """
+        f = self.f
+        net = self.net
+        weight_input = self.weight_input
+        weight_hidden = self.weight_hidden
+
+        input_layer = x
+        input_net = [net(weight_input[j], x) for j in range(self.J)]
+        hidden_layer = [f(input_net[j]) for j in range(self.J)]
+
+        hidden_net = [net(weight_hidden[m], hidden_layer) for m in range(self.M)]
+        output = [f(hidden_net[m]) for m in range(self.M)]
+        return input_net, hidden_layer, hidden_net, output
+
+    def calculate_error(self, t, output, input_net, hidden_net):
+        """
+        Оценка ошибок нейронов выходного и скрытого слоя
+        :param t: желаемый выход
+        :param output: получившийся выход
+        :param input_net: комбинированный вход на нейроны скрытого слоя
+        :param hidden_net: комбинированный вход на нейронны выходного слоя
+        :return: ошибки выходного и скрытого слоя
+        """
+
+        output_error = [self.df(hidden_net[m]) * (t[m] - output[m]) for m in range(self.M)]
+        hidden_error = [self.df(input_net[j]) *
+                        sum(self.weight_hidden[m][j] * output_error[m] for m in range(self.M))
+                        for j in range(self.J)]
+
+        return output_error, hidden_error
+
+    def weight_correcting(self, output_error, hidden_error, hidden_layer):
+        """
+        Настройка весов
+        :param output_error: ошибка выходного слоя
+        :param hidden_error: ошибка скрытого слоя
+        :param hidden_layer:
+        :return:
+        """
+
+        self.weight_hidden = [[self.weight_hidden[y][x] + self.learn_k * output_error[y] * hidden_layer[x]
+                               for x in range(self.J)] for y in range(self.M)]
+
+        self.weight_input = [[self.weight_input[y][x] + self.learn_k * hidden_error[y] * self.x[x]
+                              for x in range(self.N)] for y in range(self.J)]
+
+    def net(self, weight, x, w0=1):
+        """
+        :param weight: весовые коэффициенты
+        :param x: входной сигнала
+        :param w0: вес смещения
+        :return: Комбинированный вход нейрона
+        """
+        return sum(weight[i] * x[i]
+                   for i in range(len(weight))) + w0
+
+    def learning(self):
+        """
+        Процесс обучения
+        """
+
+        error = 1
+        while error > 0.01:
+            input_net, hidden_layer, hidden_net, output = self.calculate_output(self.x)
+            # print("CALCULATE OUTPUT", input_net, hidden_layer, hidden_net, output)
+
+            output_error, hidden_error = self.calculate_error(self.t, output, input_net, hidden_net)
+            # print("CALCULATE ERROR", output_error, hidden_error)
+
+            error = self.error(output, self.t)
+            print("Ошибка({}) = {}, выход = {}".format(self.epoch, error, output))
+
+            self.epoch += 1
+            self.weight_correcting(output_error, hidden_error, hidden_layer)
+            # print("WEIGHT CORRECTING", self.weight_input, self.weight_hidden)
+
+    def error(self, y, y_true):
+        """
+        Функция суммарной среднеквадратичной ошибки
+        :param y: спрогнозированные значения
+        :param y_true: реальные значения
+        :return: Суммарная среднеквадратичная ошибка
+        """
+        return math.sqrt(sum((y[i] - y_true[i]) ** 2 for i in range(len(y))))
